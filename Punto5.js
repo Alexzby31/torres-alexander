20e9d3060a3782aff00e4a0487ea4649c891fe54
Alexzby31 <166080763+Alexzby31@users.noreply.github.com>
2024-04-10 22:30:34 -0500

    Add files via upload

---
+++ Punto5.js
@@ -0,0 +1,10 @@
+// Declarar una variable tipo objeto y asignarle valores
+let miObjeto = {
+    numero: 42,
+    cadena: "Hola",
+    booleano: true,
+    objetoVacio: {}
+};
+
+// Imprimir el objeto en la consola
+console.log(miObjeto);
